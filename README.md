5933360b237bf7bc7ec4a5042bc58f5af7fef7ed
junting <junting0218@gmail.com>
2025-05-01 01:59:43 +0800

    init java sample application

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cloud-Native-HW4
+This is assignment 4 for Cloud-Native
---
+++ src/main/java/com/tsmc/cloudnative/Main.java
@@ -0,0 +1,21 @@
+package com.tsmc.cloudnative;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("This is a Cloud-Native-HW4 application.");
+
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
